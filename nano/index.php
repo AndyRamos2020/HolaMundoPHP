b73c01e7168df89ab9ec66874ca352f94a0eaf37
AndyRamos2020 <andy.ramos01gg@gmail.com>
2025-03-24 13:37:23 -0600

    Add files via upload

---
+++ nano/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola Mundo</title>
+</head>
+<body>
+    <h1>Hola mundo, soy Andy Ramos</h1>
+    <img src="imagen_ia.jpg" alt="Imagen generada con IA" width="400">
+</body>
+</html>
